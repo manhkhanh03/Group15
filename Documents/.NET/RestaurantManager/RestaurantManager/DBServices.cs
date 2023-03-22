54396431addb9d37e37d0be73c856cb3b6df4934
NganOC <Ngan71567@gmail.com>
2023-03-22 09:50:28 +0700

    ngan add typefoods form

--- Documents/.NET/RestaurantManager/RestaurantManager/DBServices.cs
+++ Documents/.NET/RestaurantManager/RestaurantManager/DBServices.cs
@@ -11,7 +11,7 @@ namespace RestaurantManager
 {
     internal class DBServices
     {
-        private string conn = @"Data Source=LAPTOP-VL2DNGOC\SQLEXPRESS;Initial Catalog=RestaurantManager;Integrated Security=True";
+        private string conn = @"Data Source=LAPTOP-VL2DNGOC\SQLEXPRESS;Initial Catalog=RestaurantManager;User ID=manhkhanh;Password=1234";
         private SqlConnection mySqlConnection;
 
         public DBServices()
